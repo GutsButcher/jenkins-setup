fbb2c3ba84c4b8398a71b7830647c7040eb50fab
local <abdulla.amash@gmail.com>
2025-02-13 16:06:45 +0300

    Integrate Ansible & Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,11 +22,13 @@ resource "tls_private_key" "jenkins_key" {
 # save the key pair in a local file
 resource "local_file" "jenkins_key" {
   content  = tls_private_key.jenkins_key.private_key_pem
-  filename = "./key/jenkins_key"
+  filename = "./key/${var.key_name}"
+  # set the file permissions to 0600
+  file_permission = 0600
 }
 resource "local_file" "jenkins_key_pub" {
   content  = tls_private_key.jenkins_key.public_key_openssh
-  filename = "./key/jenkins_key.pub"
+  filename = "./key/${var.key_name}.pub"
 }
 resource "aws_key_pair" "jenkins_key" {
   key_name   = var.key_name
@@ -62,13 +64,13 @@ resource "aws_security_group" "jenkins" {
 resource "aws_vpc_security_group_ingress_rule" "jenkins" {
   description       = "Allow all ports from the same security group"
   security_group_id = aws_security_group.jenkins.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = -1
 }
 resource "aws_vpc_security_group_egress_rule" "jenkins" {
   description       = "Allow all ports from the same security group"
   security_group_id = aws_security_group.jenkins.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = -1
 }
 ###############################################################
@@ -85,7 +87,50 @@ resource "aws_instance" "jenkins" {
     lifecycle {
     ignore_changes = [security_groups]
   }
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    EOF
+  
+}
 
+
+
+
+
+# pass the public IP of the instance to ansible using local-exec with EOF to a file named inventory
+resource "null_resource" "jenkins" {
+  provisioner "local-exec" {
+    
+    command = <<-EOF
+      mkdir -p ../ansible 
+      mkdir ../ansible/host_vars || rm -fr ../ansible/host_vars/*
+      touch ../ansible/host_vars/${aws_instance.jenkins.public_ip}.yml
+      inventory=$(realpath ../ansible/inventory)
+      cfg=$(realpath ../ansible/ansible.cfg)
+      echo "[defaults]" > $cfg
+      echo "inventory = inventory" >> $cfg
+      echo "remote_user = ubuntu" >> $cfg
+      echo "[jenkins]" > $inventory
+      echo "${aws_instance.jenkins.public_ip}" >> $inventory
+      echo "[jenkins:vars]" >> $inventory
+      echo "ansible_ssh_user=ubuntu" >> $inventory
+      echo "ansible_ssh_private_key_file=$(realpath ./key/${var.key_name})" >>  $inventory
+    EOF
+  }
+}
+
+
+
+
+
+
+output "public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+# print an ssh command to connect to the instance
+output "ssh_command" {
+  value = "ssh -i ./key/${var.key_name} ubuntu@${aws_instance.jenkins.public_ip}"
 }
 
 
